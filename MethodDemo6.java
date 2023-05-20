ef00dd23ddbb40d5d42ce3cd323db22298a4c3cc
정훈 <133840938+DoggleDoggle@users.noreply.github.com>
2023-05-20 18:40:23 +0900

    Add files via upload

---
+++ MethodDemo6.java
@@ -0,0 +1,25 @@
+package org.opentutorials.javatutorials.method;
+
+public class MethodDemo6 {
+	
+    public static String numbering (int init, int limit) {
+    	
+    	int i = init;
+    	String output = "";
+    	while (i < limit) {
+    		
+    		output += i;
+    		i++;
+    		
+    	}
+    	return "Good!";
+    }
+       
+    public static void main(String[] args) {
+		
+    	String result = numbering(1, 5);
+    	System.out.println(result);
+    }
+    	
+	}
+      
